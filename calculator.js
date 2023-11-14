60c31083f34da3d732aec281ce6821d5f1d698b3
AyubElmiSilver <09ayub10@gmail.com>
2023-11-14 13:57:34 +0000

    Select and Create elements using DOM manipulation.
    The code will select elements and create buttons with an assigned value.

--- calculator.js
+++ calculator.js
@@ -22,4 +22,19 @@ function operate (firstnum, operator, secondnum ) {
     : "Enter a number, operator and another number";
 }
 
-console.log(operate(79, "-", 5));
+//console.log(operate(79, "-", 5));
+
+const calculator = document.querySelector(".calculator");
+const container = document.querySelector(".last-buttons-container");
+const secondbox = document.querySelector(".secondBox")
+
+
+for (let i = 0; i < 16; i++) {
+    const mainbox = document.createElement("button");
+    mainbox.setAttribute("id", "buttons-mainbox");
+    let mainboxInput = ["AC", "+/-", "%", "/", 7, 8, 9, "*", 4, 5, 6, "-", 1, 2, 3, "+"];
+    mainbox.textContent = mainboxInput[i];
+    mainbox.style.cssText = "width: 97px; height: 85px; font-size: 40px; font-weight: 700;";
+    secondbox.appendChild(mainbox);
+}
+
